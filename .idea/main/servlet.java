4d6525ed4a4f32436a7c903201dddf36cabc62e4
hammdata <gohammdata@gmail.com>
2020-03-22 20:50:03 -0400

    Main Server Framework

---
+++ .idea/main/servlet.java
@@ -0,0 +1,14 @@
+import javax.servlet.*;
+import javax.servlet.http.*;
+import java.IO.*;
+
+public class MyServelet extends HttpServlet {
+    public init() {
+        // Perform any initialization instances
+    }
+    public void doGet(HttpServletRequest re, HttpServletResponse resp)
+        throws ServletException, IOException {
+
+        // Proces the request and generate the resonse
+    }
+}
